96b89ba4eace28d5dc48d852243ac5f669ffe423
Andy Wolff <andy.wolff@gmail.com>
2016-11-25 14:44:05 -0600

    works

--- server/app.js
+++ server/app.js
@@ -5,15 +5,15 @@ var data = require("./public/data/sigma");
 
 app.set("port", (process.env.PORT || 5000));
 
-app.get("/data", function(req,res){
-    res.send(data);
+app.get("/data", function (req, res) {
+	res.send(data);
 });
 
-app.get("/*", function(req,res){
-    var file = req.params[0] || "views/index.html";
-    res.sendFile(path.join(__dirname, "/public/", file));
+app.get("/*", function (req, res) {
+	var file = req.params[0] || "views/index.html";
+	res.sendFile(path.join(__dirname, "/public/", file));
 });
 
-app.listen(app.get("port"), function(){
-    console.log("Server up and running on Port", 5000);
-});
+app.listen(app.get('port'), function () {
+    console.log('Server up and running on port', app.get('port'));
+  });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,29 +1,72 @@
-var peopleArray = [];
+$(document)
+	.ready(function () {
+		$.ajax({
+				type: 'GET',
+				url: '/data',
+				success: function (data) {
+						var sigmanauts = data.sigmanauts;
+						console.log('GET/data returns ', sigmanauts);
+						var i = 0;
+						var backInt = null;
+						var aheadInt = null;
+						$('#prev')
+							.on('click', backward);
+						$('#next')
+							.on('click', forward);
+						info(sigmanauts[i]);
 
-
-$(document).ready(function () {
-	init();
-	con();
-});
-
-function init() {
-	$.ajax({
-		type: "GET",
-		url: "/data",
-		async: false,
-		success: function (data) {
-			// iterate over array of student objects and create usable objects
-			peopleArray = data.sigmanauts;
-		},
-		error: function (xhr) {
-			console.log("request failed");
-		}
-	});
-}
-// $.each(peopleArray, function (i, val) {
-// 	console.log("i " + i, "length " + "val " + val, "val.name " + val.name);
-// })
-
-function con() {
-	console.log(peopleArray);
-}
+						function info(sigmanauts) {
+							$('#name')
+								.text(sigmanauts.name);
+							$('#userName')
+								.text(sigmanauts.git_username);
+							$('#shout')
+								.text(sigmanauts.shoutout);
+							// $( '.text' )
+							//  .fadeIn( 'fast' );
+						} // end function info
+						function forward() {
+							if (i === sigmanauts.length - 1) {
+								i = 0;
+								info(sigmanauts[i]);
+								i++;
+								fastFoward()
+							} else {
+								i++;
+								info(sigmanauts[i]);
+								fastFoward();
+							}
+						} // end function forward
+						function backward() {
+							if (i === 0) {
+								i = sigmanauts.length - 1;
+								info(sigmanauts[i]);
+								rewind()
+							} else {
+								i--;
+								info(sigmanauts[i]);
+								rewind()
+							}
+						} // end function backward
+						function rewind() {
+							if (backInt) {
+								clearInterval(backInt);
+								clearInterval(aheadInt);
+								// $( '.text' )
+								//  .fadeOut( 'slow' );
+							}
+							backInt = setInterval(backward, 9000);
+						} // end function rewind
+						function fastFoward() {
+							if (aheadInt) {
+								clearInterval(aheadInt);
+								clearInterval(backInt);
+								// $( '.text' )
+								//  .fadeOut( 'slow' );
+							}
+							aheadInt = setInterval(forward, 9000);
+						}
+					} // end function fastFoward
+			}) // end function success
+	}); // end ajax
+// end ready function
